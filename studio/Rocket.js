c82f2e9b5eb783d68097d1991d4aca6fee542f15
AlphieKiu <kiuhuaien87@gmail.com>
2019-10-19 17:49:43 -0500

    finished studio

---
+++ studio/Rocket.js
@@ -0,0 +1,48 @@
+"use strict";
+exports.__esModule = true;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var sumOfItems = 0;
+        for (var i = 0; i < items.length; i++) {
+            sumOfItems += Number(items[i].massKg);
+        }
+        return sumOfItems;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
